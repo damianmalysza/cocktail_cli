f3481dd4ba265b68912e273c84da312c62e5e777
Damian Malysza <damian.malysza@gmail.com>
2020-11-23 20:05:54 -0500

    added intro artwork

--- lib/cli_controller.rb
+++ lib/cli_controller.rb
@@ -1,7 +1,12 @@
-class CLIController
+require_relative "../config/environment"
 
+class CLIController
   def call
-    puts "hello world"
+    puts self.intro_artwork
+  end
+
+  def intro_artwork
+    File.readlines("./lib/cocktail_art.txt") {|line| puts line}
   end
 
 end
