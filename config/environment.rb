9d9625a6a16e1e87c3a6e7c1fefdab9361447e7b
Damian Malysza <damian.malysza@gmail.com>
2020-11-23 19:29:11 -0500

    added additional classes to be used and loaded into environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require "httparty"
 
 require_relative "../lib/cocktail_cli"
 require_relative "../lib/drink"
+require_relative "../cli_controller"
+require_relative "../scraper"
---
+++ lib/cli_controller.rb
@@ -0,0 +1,3 @@
+class CLIController
+
+end
