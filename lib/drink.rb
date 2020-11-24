4f5a6f91a547d0c98bf65adc60f819c90bebbf48
Damian Malysza <damian.malysza@gmail.com>
2020-11-23 22:13:38 -0500

    added environment requirements for drink and scraper classes

--- lib/drink.rb
+++ lib/drink.rb
@@ -1,9 +1,17 @@
+require_relative "../config/environment"
+
 class Drink
-  attr_accessor :name, :ingredients
+  attr_accessor :name, :ingredients, :instructions
+
+  @@potential_ingredients = []
 
   def initialize(name)
     @name = name
+    @ingredients = {}
+    @instructions = nil
   end
+
+
 
 
 end
