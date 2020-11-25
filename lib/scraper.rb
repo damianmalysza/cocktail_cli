8f6d0c9b38bdbcfe5c4bd039d8ca00f4f8272e07
Damian Malysza <damian.malysza@gmail.com>
2020-11-25 17:09:43 -0500

    added index_cocktails instance method logic to Scraper class.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,15 +3,20 @@ require_relative "../config/environment"
 class Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://uk.thebar.com/cocktail-recipes?seeAll=true"))
+    Nokogiri::HTML(URI.open("https://uk.thebar.com/cocktail-recipes?seeAll=true"))
   end
 
   def index_cocktails
-    self.get_page.css(".recipe-tile")
+    self.get_page.css(".recipe-tile").css("a")
   end
 
   def make_cocktail_objects
-
+    self.index_cocktails.each do |cocktail|
+          # iterate through index_cocktails
+    # grab the URL, name, and difficulty and instantiate new instances of the drink class
+    end
   end
 
 end
+
+Scraper.new.index_cocktails
